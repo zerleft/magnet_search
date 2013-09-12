a23fe476f410b24e2392730c0767304327dcbe2f
luwei <zerleft@126.com>
2013-09-12 09:09:06 +0800

    modified:   bencode.py

--- bencode.py
+++ bencode.py
@@ -11,43 +11,31 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 '''
-判断变量类型
-'''
-is_int = lambda i : type(i) is types.IntType
-
-is_str = lambda s : type(s) is types.StringType
-
-is_dict = lambda d : type(d) is types.DictType
-
-'''
 b编码
 因为这个爬虫只需要发送find_node和ping
 所以只处理这两种情况
 '''
 def bencode(dic):
-    if is_dict(dic):
-        try:
-            if dic['q'] == 'ping':
-                return 'd1:ad2:id20:' + \
-                        dic['a']['id'] + \
-                        'e1:q4:ping1:t' + \
-                        str(len(dic['t'])) + \
-                        ':' + dic['t'] + \
-                        '1:y1:qe'
+    try:
+        if dic['q'] == 'ping':
+            return 'd1:ad2:id20:' + \
+                    dic['a']['id'] + \
+                    'e1:q4:ping1:t' + \
+                    str(len(dic['t'])) + \
+                    ':' + dic['t'] + \
+                    '1:y1:qe'
 
-            if dic['q'] == 'find_node':
-                return 'd1:ad2:id20:' + \
-                       dic['a']['id'] + \
-                       '6' + ':target20:' + \
-                       dic['a']['target'] + \
-                       'e1:q9:find_node1:t' + \
-                       str(len(dic['t'])) + \
-                       ':' + dic['t'] + \
-                       '1:y1:qe'
-        except:
+        if dic['q'] == 'find_node':
+            return 'd1:ad2:id20:' + \
+                   dic['a']['id'] + \
+                   '6' + ':target20:' + \
+                   dic['a']['target'] + \
+                   'e1:q9:find_node1:t' + \
+                   str(len(dic['t'])) + \
+                   ':' + dic['t'] + \
+                   '1:y1:qe'
+    except ValueError:
             return ''
-    else:
-        return ''
 
 '''
 解码
