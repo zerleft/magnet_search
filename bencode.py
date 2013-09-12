bff4de1c0940b7a77b9fbafbd25a9010eaa56d46
luwei <zerleft@126.com>
2013-09-12 10:12:00 +0800

    modified:   bencode.py

--- bencode.py
+++ bencode.py
@@ -12,34 +12,40 @@ sys.setdefaultencoding('utf-8')
 
 '''
 b编码
-因为这个爬虫只需要发送find_node和ping
-所以只处理这两种情况
 '''
-def bencode(dic):
+def req_ping_encode(dic):
     try:
-        if dic['q'] == 'ping':
-            return 'd1:ad2:id20:' + \
-                    dic['a']['id'] + \
-                    'e1:q4:ping1:t' + \
-                    str(len(dic['t'])) + \
-                    ':' + dic['t'] + \
-                    '1:y1:qe'
-
-        if dic['q'] == 'find_node':
-            return 'd1:ad2:id20:' + \
-                   dic['a']['id'] + \
-                   '6' + ':target20:' + \
-                   dic['a']['target'] + \
-                   'e1:q9:find_node1:t' + \
-                   str(len(dic['t'])) + \
-                   ':' + dic['t'] + \
-                   '1:y1:qe'
+        #发送ping编码
+        if dic.has_key('q') and dic['q'] == 'ping':
+            return 'd1:ad2:id20:' + dic['a']['id'] + \
+                    'e1:q4:ping1:t' + str(len(dic['t'])) + \
+                    ':' + dic['t'] + '1:y1:qe'
     except ValueError:
-            return ''
+        return ''
+
+def req_find_node_encode(dic):
+    try:
+        #发送find_node编码
+        if dic.has_key('q') and dic['q'] == 'find_node':
+            return 'd1:ad2:id20:' + dic['a']['id'] + \
+                   '6' + ':target20:' + dic['a']['target'] + \
+                   'e1:q9:find_node1:t' + str(len(dic['t'])) + \
+                   ':' + dic['t'] + '1:y1:qe'
+    except ValueError:
+        return ''
+
+def req_get_peer_encode(dic):
+    try:
+        #发送get_peer编码
+        if dic.has_key('q') and dic['q'] == 'get_peer':
+            return 
+    except ValueError:
+        return ''
 
 '''
 解码
 '''
-'''def bdecode(string):
-    if is_str(string):
-        try:'''
+def bdecode(string):
+    res = {}
+    try:
+         
